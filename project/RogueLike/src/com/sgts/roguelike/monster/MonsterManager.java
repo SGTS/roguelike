4bd0fab6b7eda84c2f146980d6cdd104bffc90e2
Daniel-Gunn <daniel.taz.gunn@gmail.com>
2014-05-13 12:23:42 +1200

    Removed the confusing monster structure and replaced it with a single Monster Manager.

--- project/RogueLike/src/com/sgts/roguelike/monster/MonsterManager.java
+++ project/RogueLike/src/com/sgts/roguelike/monster/MonsterManager.java
@@ -1,9 +1,12 @@
 package com.sgts.roguelike.monster;
 
+import java.util.Random;
+
 import com.sgts.roguelike.components.Movement;
 import com.sgts.roguelike.components.Renderable;
 import com.sgts.roguelike.components.Stat;
 import com.sgts.roguelike.entity.Entity;
+import com.sgts.roguelike.entity.EntityManager;
 
 /**
  * 
@@ -15,44 +18,38 @@ import com.sgts.roguelike.entity.Entity;
  */
 
 public class MonsterManager {
-
-	//Variables
-	private Entity monster = null;
 	
 	//Constructor
 	public MonsterManager() {
-		this.monster = Entity.createEntity();
-		this.monster.addComponent(Renderable.createComponent());
-		this.monster.addComponent(Stat.createComponent());
-		this.monster.addComponent(Movement.createComponent());
+		
 	}
 	
-	//Getters and Setters
-	public Entity getMonster() {
+	//Base Create Monster function that returns a default monster
+	public Entity createMonster() {
+		
+		Entity monster = EntityManager.createEntity();
+		
+		monster.addComponent(Renderable.createComponent());
+		monster.addComponent(Stat.createComponent());
+		monster.addComponent(Movement.createComponent());
+		
 		return monster;
-	}
-	public void setMonster(Entity newMonster) {
-		this.monster = newMonster;
-	}
-
-	//Generates a blank monster for custom creation
-	public Entity generateBlankMonster() {
 		
-		return this.monster;
 	}
 	
-	//Call this to generate a new monster
-	public Entity generateMonster() {
+	public Entity createRandomMonster() {
 		
+		Random rand = new Random();
+		Entity monster = EntityManager.createEntity();
 		
-		Entity tempMonster = monster;
-		cleanMonster();
-		return tempMonster;
-	}
-	
-	//Function called to clear out the monster so it will not remain the same monster after each call.
-	private void cleanMonster() {
-		this.monster = null;
+		monster.addComponent(Renderable.createComponent());
+		monster.addComponent(Stat.createComponent());
+		monster.addComponent(Movement.createComponent());
+		
+		//Add random stats to the monster
+		
+		return monster;
+		
 	}
 	
 }
